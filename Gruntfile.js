d390e590c1a7d7573468df66d13d81589ff3433c
lin04com <ahschl0322@gmail.com>
2013-08-27 22:51:22 +0800

    add tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,6 +4,27 @@ module.exports = function(grunt) {
   var text = transport.text.init(grunt);
   var script = transport.script.init(grunt);
 
+  var formatter = function(hash){
+      var reg = /\\/g;
+      var result = ''; //
+      var val;
+      for(var key in hash){
+          //val = hash[key];
+          val = hash[key];
+          var file = key.replace(reg,'\/');
+          var arr = file.split('.');
+          arr[arr.length-2] = arr[arr.length-2] + '_' + val;
+          val = arr.join('.');
+          result += '["'+file + '" , "'+ val + '"],'; //\n
+      }
+      var lastIndex = result.lastIndexOf(',');
+      if (lastIndex > -1) {
+          result = result.substring(0, lastIndex);
+      }
+      //result += '\n];';
+      return result;
+  };
+
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
 
@@ -68,6 +89,28 @@ module.exports = function(grunt) {
       }
     },
 
+   min: {
+      'dist': {
+        'src': ['compiler/src/**/*.js'],
+        'dest': 'compiler/dist/aa-min.js'
+      }
+    },
+
+    cachebuster: {
+      build: {
+          options: {
+              banner: 'var fileMap = ',
+              //format: 'json',
+              basedir: 'src/',
+              hashTail : true,
+              length : 8,
+              complete : formatter
+          },
+          src: ['src/**/*.js', '!src/**/*-debug.js'],
+          dest: 'dist/filemap.js'
+      }
+    },
+
     clean: {
       build : {
         src: ['.build', '.*']
@@ -81,12 +124,18 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-cmd-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-yui-compressor');
+  grunt.loadNpmTasks('grunt-cachebuster');
 
   grunt.registerTask('compiler', ['uglify:compiler']);
 
+  grunt.registerTask('yuicompressor', ['min']);
+
+  grunt.registerTask('cache', ['cachebuster']);
+
   grunt.registerTask('default', ['transport', 'concat']);
 
-  grunt.registerTask('build', ['transport:build', 'concat:build', 'uglify:build', 'clean']);
+  grunt.registerTask('build', ['transport:build', 'concat:build', 'uglify:build', 'cachebuster', 'clean']);
 
   grunt.registerTask('clear', ['clean']);
 
